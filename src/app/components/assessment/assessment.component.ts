26e2a48483a6ee88339d8beb966b4ba68ac45de3
NakoNachev <nako.n.nachev@gmail.com>
2022-12-01 18:52:41 +0100

    fix mappings

--- src/app/components/assessment/assessment.component.ts
+++ src/app/components/assessment/assessment.component.ts
@@ -3,6 +3,9 @@ import officeData from '../../data/multiple-choice/office.json'
 import travelData from '../../data/multiple-choice/travel.json'
 import travelData2 from '../../data/multiple-choice/travel2.json'
 import florfaunData from '../../data/match-sentence/flora_fauna.json'
+import floraFaunaMCData from '../../data/mc-quiz/flora-fauna.json'
+import geoDataMc from '../../data/multiple-choice/geography_weather.json'
+import geoDataMs from '../../data/match-sentence/geography_weather_match_sentence.json'
 @Component({
   selector: 'app-assessment',
   templateUrl: './assessment.component.html',
@@ -18,7 +21,10 @@ export class AssessmentComponent implements OnInit {
     { id: 1, label: "Multiple choice 1 (travel)", type: "multiple-choice", key: 'mc-travel' },
     { id: 2, label: "Multiple choice 2 (travel)", type: "choose-definition", key: 'def-travel' },
     { id: 3, label: "Multiple choice 3 (office)", type: "multiple-choice", key: 'mc-office' },
-    { id: 4, label: "Match sentence (Flora and Fauna)", type: "match-sentence", key: 'ms-florfaun' },
+    { id: 4, label: "Multiple choice 4 (geography)", type: "multiple-choice", key: 'mc-geo' },
+    { id: 5, label: "Multiple choice 5 (flora and fauna)", type: "multiple-choice", key: 'mc-florafauna' },
+    { id: 6, label: "Match sentence (Flora and Fauna)", type: "match-sentence", key: 'ms-florfaun' },
+    { id: 7, label: "Match sentence (Geography)", type: "match-sentence", key: 'ms-geo' },
 
   ]
   constructor() { }
@@ -30,16 +36,25 @@ export class AssessmentComponent implements OnInit {
     this.currentGameId = id
     switch (id) {
       case 1:
-        this.gameData = this.isLearnAssessment ? travelData.slice(0,2) : travelData
+        this.gameData = this.isLearnAssessment ? travelData.slice(0, 2) : travelData
         break
       case 2:
-        this.gameData = this.isLearnAssessment ? travelData2.slice(0,2) : travelData2
+        this.gameData = this.isLearnAssessment ? travelData2.slice(0, 2) : travelData2
         break
       case 3:
-        this.gameData = this.isLearnAssessment ? officeData.slice(0,2) : officeData
+        this.gameData = this.isLearnAssessment ? officeData.slice(0, 2) : officeData
         break
       case 4:
+        this.gameData = this.isLearnAssessment ? geoDataMc.slice(0, 2) : geoDataMc
+        break
+      case 5:
+        this.gameData = this.isLearnAssessment ? floraFaunaMCData.slice(0, 2) : floraFaunaMCData
+        break
+      case 6:
         this.gameData = florfaunData
+        break
+      case 7:
+        this.gameData = geoDataMs
         break
     }
   }
--- src/app/components/explanations/explanations.component.ts
+++ src/app/components/explanations/explanations.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import officeData from '../../data/multiple-choice/office.json'
-import travelData from '../../data/multiple-choice/travel.json'
-import travelData2 from '../../data/multiple-choice/travel2.json'
-import florfaunData from '../../data/match-sentence/flora_fauna.json'
 @Component({
   selector: 'app-explanations',
   templateUrl: './explanations.component.html',
@@ -10,37 +6,9 @@ import florfaunData from '../../data/match-sentence/flora_fauna.json'
 })
 export class ExplanationsComponent implements OnInit {
 
-  public gameChoice
-  public gameData
-  public currentGameId: number
-  public gameChoiceValue = [
-    { id: 1, label: "Multiple choice 1 (travel)", type: "multiple-choice", key: 'mc-travel' },
-    { id: 2, label: "Multiple choice 2 (travel)", type: "choose-definition", key: 'def-travel' },
-    { id: 3, label: "Multiple choice 3 (office)", type: "multiple-choice", key: 'mc-office' },
-    { id: 4, label: "Match sentence (Flora and Fauna)", type: "match-sentence", key: 'ms-florfaun' },
-
-  ]
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  public setGameData(id) {
-    this.currentGameId = id
-    switch (id) {
-      case 1:
-        this.gameData = travelData
-        break
-      case 2:
-        this.gameData = travelData2
-        break
-      case 3:
-        this.gameData = officeData
-        break
-      case 4:
-        this.gameData = florfaunData
-        break
-    }
   }
 
 }
